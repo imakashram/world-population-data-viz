8ff50eaff7f7af3e4b99865f7917259557ea39d3
imakashram <mail2akashram@gmail.com>
2023-11-01 14:57:21 +0530

    area chart created

--- src/app/components/area-chart/area-chart.component.ts
+++ src/app/components/area-chart/area-chart.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, ViewContainerRef } from '@angular/core';
 // d3.js module import
-import { select,selectAll } from 'd3-selection';
-import { scaleLinear,scaleTime } from 'd3-scale';
+import { select } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
 import {extent,min,max} from 'd3-array';
-import { axisBottom,axisLeft } from 'd3-axis';
-import { area } from 'd3-shape'
+import { axisBottom} from 'd3-axis';
+import { line,area } from 'd3-shape'
+
 
 @Component({
   selector: 'app-area-chart',
@@ -14,19 +15,19 @@ import { area } from 'd3-shape'
 export class AreaChartComponent {
   @Input() public containerWidth! : number   // total containerWidth including margins
   @Input() public containerHeight! : number  // total containerHeight including margins
-  @Input() public chartData! : Array<ChartDataInterface>   // required chartData to plot
+  @Input() public chartData! : Array<ChartDataInterface>   // required chartData 
 
   private svgInHtml! : d3.Selection<d3.BaseType, unknown, null, undefined>
   private chartGroup! : d3.Selection<SVGGElement, unknown, null, undefined>
   private margin = {
-                      top: 10, 
-                      right: 10, 
-                      bottom: 50, 
-                      left: 50
+                      top: 20, 
+                      right: 25, 
+                      bottom: 30, 
+                      left: 30
                     }
   private chartWidth! : number
   private chartHeight! : number
-  private xScale! : d3.ScaleTime<number, number, never>
+  private xScale! : d3.ScaleLinear<number, number, never>
   private yScale! : d3.ScaleLinear<number, number, never>
   private dataToPlot! : Array<DataToPlotInterface>
   
@@ -49,6 +50,7 @@ export class AreaChartComponent {
     // set the dimensions and margins of the graph
     // chart width after margin-left and margin-right
     this.chartWidth =  this.containerWidth - this.margin.left - this.margin.right;  
+
     // chart height after margin-top and margin-bottom 
     this.chartHeight = this.containerHeight - this.margin.top - this.margin.bottom;  
 
@@ -60,8 +62,8 @@ export class AreaChartComponent {
 
     // create a group within the svg element to apply margins
     this.chartGroup = this.svgInHtml
-                        .append("g")
-                        .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
+                          .append("g")
+                          .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
                       
    }
 
@@ -70,12 +72,14 @@ export class AreaChartComponent {
      this.configureChartData();
      this.drawXAxis();
      this.drawYAxis();
+     this.drawLine();
      this.drawArea();
+     this.printYLabel();
    }
 
 
   /**
-   * @description remove extisting scatterplot chart from DOM
+   * @description remove extisting area chart from DOM
    * @function removeAreaChart
    */
   private removeAreaChart() : void {
@@ -91,7 +95,7 @@ export class AreaChartComponent {
   private configureChartData() : void {
     this.dataToPlot = this.chartData.map( (d: ChartDataInterface) => {
       return {
-          Year : new Date (`${d.Year}`),
+          Year : d.Year,
           TotalPopulation  :  d.TotalPopulation,
           Population_Bn : d.TotalPopulation/1000000,
       };
@@ -104,24 +108,35 @@ export class AreaChartComponent {
    */
   private drawXAxis() : void { 
     // find xDomain Array [min,max]
-    const xDomain = extent(this.dataToPlot, (d: DataToPlotInterface) => d.Year) as any
+    const xDomain = extent(this.dataToPlot, (d: DataToPlotInterface) => d.Year) as [number,number]
+
     // find xRange [0, chartWidth]
     const xRange = [0, this.chartWidth];
 
+
     // add xScale
-    this.xScale = scaleTime()
+    this.xScale = scaleLinear()
                      .domain(xDomain)
                      .range(xRange);
 
+
     // add xAxis
     const xAxis = axisBottom(this.xScale)
-                  //.tickFormat((d:any) => d === xDomain[1] ? `>${d}` : d)
+                    // print only first and last tickValues
+                    .tickValues([this.xScale.domain()[0], this.xScale.domain()[1]])
+                    .tickFormat((d: any) => d);
                  
     
     this.chartGroup.append("g")
                   .attr("class", "x-axis")
+                  .style("font-size", "14")
+                  .attr("font-weight","bold")
                   .attr("transform", `translate(0,${this.chartHeight})`)
                   .call(xAxis)
+
+    // Select and remove the axis lines
+    this.chartGroup.select(".domain").remove();
+
    }
 
   /**
@@ -137,16 +152,29 @@ export class AreaChartComponent {
     const yRange = [this.chartHeight,0];
 
     // add yScale
+    // take yDomainMin is 0 so htat chart will not start from given data min value
     this.yScale = scaleLinear()
-                     .domain([yDomainMin,yDomainMax])
+                     .domain([0,yDomainMax])
                      .range(yRange);
+  }
 
-    // add yAxis
-    const yAxis = axisLeft(this.yScale)
 
-    this.chartGroup.append("g")
-                  .attr("class", "y-axis")
-                  .call(yAxis)
+     /**
+   * @description draw line
+   * @function drawLine
+   */
+     private drawLine(): void { 
+      // line generator
+      let lineGenerator = line()
+                          .x((d:any)=> this.xScale(d.Year))
+                          .y((d:any)=> this.yScale(d.Population_Bn));
+
+      this.chartGroup.append("path")
+                     .datum(this.dataToPlot)
+                     .attr("d", lineGenerator as any)
+                     .attr("stroke", "#FF9C05")
+                     .attr("stroke-width", 2)
+                     .attr("fill", "none");
   }
 
 
@@ -155,18 +183,35 @@ export class AreaChartComponent {
    * @function drawArea
    */
   private drawArea() : void {
-  // add circle
-  this.chartGroup.append('g')
-                 .append("path")
-                 .datum(this.dataToPlot)
-                 .attr("fill", "#cce5df")
-                 .attr("stroke", "#69b3a2")
-                 .attr("stroke-width", 1.5)
-                 .attr("d", area()
-                  .x((d:any )=> this.xScale(d.Year))
-                  .y0(this.yScale(0))
-                  .y1((d:any) => this.yScale(d.Population_Bn)) as any
-                 )
+
+    // create area generator 
+    let areaGenerator : d3.Area<[number, number]> = area()
+                                                    .x((d:any)=>  this.xScale(d.Year))
+                                                    .y0(this.chartHeight)
+                                                    .y1((d: any)=> this.yScale(d.Population_Bn));
+      // add path and area
+      this.chartGroup.append('g')
+                     .append("path")
+                     .datum(this.dataToPlot)
+                     .attr("fill", "#ffddb5")
+                     .attr("opacity",0.5)
+                     .attr("d", areaGenerator as any)
+  }
+
+  private printYLabel() : void { 
+    // adding y value labels as text
+    this.chartGroup.selectAll(".y-label")
+    .data(this.dataToPlot)
+    .join("text")
+    .attr("class", "y-label")
+    .text((d:DataToPlotInterface,i:number) => (i === 0 || i === this.dataToPlot.length-1) ? `${d.Population_Bn.toFixed(2)}Bn` : null)
+    .attr("x", (d:DataToPlotInterface) => this.xScale(d.Year)) 
+    .attr("y", (d:DataToPlotInterface) => this.yScale(d.Population_Bn))
+    .attr("dy",-10)
+    .attr("dx",2)
+    .attr("font-size", "14")
+    .attr("font-weight","bold")
+    .attr("text-anchor", "middle");
   }
 }
 
@@ -175,7 +220,7 @@ export interface ChartDataInterface {
   TotalPopulation: number
 }
 export interface DataToPlotInterface {
-  Year: Date
+  Year: number
   TotalPopulation: number
   Population_Bn : number
 }
